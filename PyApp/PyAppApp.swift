fefe4dda6eaf3be57dca79408a862d003136aa4b
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-05-18 13:13:53 -0300

    Initial Commit

---
+++ PyApp/PyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PyAppApp.swift
+//  PyApp
+//
+//  Created by Bruno Oliveira on 18/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
